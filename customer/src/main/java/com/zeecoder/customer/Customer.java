b0866427707f4e53dc3f0c3504b6927ac216b477
ihor <i.zaporozhtsev@gmail.com>
2022-10-29 20:14:11 +0300

    add postgres implement simple data storage

--- customer/src/main/java/com/zeecoder/customer/Customer.java
+++ customer/src/main/java/com/zeecoder/customer/Customer.java
@@ -1,12 +1,24 @@
 package com.zeecoder.customer;
 
-import lombok.Builder;
-import lombok.Data;
+import lombok.*;
+import javax.persistence.*;
 
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
 public class Customer {
 
+    @Id
+    @SequenceGenerator(
+            name = "customer_id_sequence",
+            sequenceName = "customer_id_sequence"
+    )
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "customer_id_sequence"
+    )
     private Integer id;
     private String firstName;
     private String lastName;
--- customer/src/main/java/com/zeecoder/customer/CustomerService.java
+++ customer/src/main/java/com/zeecoder/customer/CustomerService.java
@@ -3,12 +3,16 @@ package com.zeecoder.customer;
 import org.springframework.stereotype.Service;
 
 @Service
-public record CustomerService() {
+public record CustomerService(CustomerRepository repository) {
     public void registerCustomer(CustomerRegistrationRequest request) {
         Customer customer = Customer.builder()
                 .firstName(request.firstName())
                 .lastName(request.lastName())
                 .email(request.email())
                 .build();
+
+        //todo: check if email valid
+        //todo check if email not taken
+        repository.save(customer);
     }
 }
