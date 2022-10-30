0529131da71610179fd9fa1769b33a508422df57
ihor <i.zaporozhtsev@gmail.com>
2022-10-30 10:32:43 +0200

    add new Fraud microservice

--- customer/src/main/java/com/zeecoder/customer/CustomerApplication.java
+++ customer/src/main/java/com/zeecoder/customer/CustomerApplication.java
@@ -3,10 +3,6 @@ package com.zeecoder.customer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-/**
- * Hello world!
- *
- */
 @SpringBootApplication
 public class CustomerApplication
 {
