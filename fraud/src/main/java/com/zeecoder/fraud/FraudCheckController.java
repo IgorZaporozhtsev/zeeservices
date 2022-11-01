8438edec1d7d59993629223ad6d64ef2ebb0ccb9
ihor <i.zaporozhtsev@gmail.com>
2022-11-01 20:04:49 +0200

    add Load balancing if we send 1 request but have multiple instance of Fraud application

--- fraud/src/main/java/com/zeecoder/fraud/FraudCheckController.java
+++ fraud/src/main/java/com/zeecoder/fraud/FraudCheckController.java
@@ -2,7 +2,10 @@ package com.zeecoder.fraud;
 
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("api/v1/fraud-check")
@@ -15,7 +18,7 @@ public class FraudCheckController {
     @GetMapping(path = "{customerId}")
     public FraudCheckResponse isFraudster(@PathVariable("customerId") Integer customerID){
         boolean isFraudulentCustomer = service.isFraudulentCustomer(customerID);
-        log.info("fraud  check request fir customer {}", customerID);
+        log.info("fraud  check request from customer {}", customerID);
         return new FraudCheckResponse(isFraudulentCustomer);
     }
 }
