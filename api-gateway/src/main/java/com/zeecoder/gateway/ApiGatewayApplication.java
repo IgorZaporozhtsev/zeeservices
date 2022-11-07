c28f7a457e8325105f0b1c4b2931c37bde966bd9
ihor <i.zaporozhtsev@gmail.com>
2022-11-07 19:42:17 +0200

    configure api-gateway for Eureka server

--- api-gateway/src/main/java/com/zeecoder/gateway/ApiGatewayApplication.java
+++ api-gateway/src/main/java/com/zeecoder/gateway/ApiGatewayApplication.java
@@ -6,9 +6,9 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableEurekaClient
-public class ApiGateWayApplication {
+public class ApiGatewayApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(ApiGateWayApplication.class, args);
+        SpringApplication.run(ApiGatewayApplication.class, args);
     }
 }
