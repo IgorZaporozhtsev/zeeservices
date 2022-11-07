4f18af8ab05ca538faec69eac10fbe1fcd6ce6f2
ihor <i.zaporozhtsev@gmail.com>
2022-11-07 13:14:21 +0200

    add Zipkin tracing and set up new API Gateway service

--- clients/src/main/java/com/zeecoder/clients/notification/NotificationClient.java
+++ clients/src/main/java/com/zeecoder/clients/notification/NotificationClient.java
@@ -2,10 +2,11 @@ package com.zeecoder.clients.notification;
 
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @FeignClient("notification")
 public interface NotificationClient {
 
     @PostMapping("api/v1/notification")
-    void sendNotification(NotificationRequest notificationRequest);
+    void sendNotification(@RequestBody NotificationRequest notificationRequest);
 }
--- fraud/src/main/java/com/zeecoder/fraud/FraudCheckHistoryRepository.java
+++ fraud/src/main/java/com/zeecoder/fraud/FraudCheckHistoryRepository.java
@@ -1,6 +1,8 @@
 package com.zeecoder.fraud;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface FraudCheckHistoryRepository extends JpaRepository<FraudCheckHistory, Integer> {
 }
