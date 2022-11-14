fe90ff0570c5b3d4f6dec6d22c142e9f40dd208c
ihor <i.zaporozhtsev@gmail.com>
2022-11-14 10:27:32 +0200

    add amqp module and AMQP Template and JacksonConverter

---
+++ amqp/src/main/java/com/zeecoder/amqp/RabbitMQConfig.java
@@ -0,0 +1,29 @@
+package com.zeecoder.amqp;
+
+import lombok.AllArgsConstructor;
+import org.springframework.amqp.core.AmqpTemplate;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.amqp.support.converter.MessageConverter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@AllArgsConstructor
+public class RabbitMQConfig {
+
+    private final ConnectionFactory connectionFactory;
+
+    @Bean
+    public AmqpTemplate amqpTemplate(){
+        RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
+        rabbitTemplate.setMessageConverter(jacksonConvertor());
+        return rabbitTemplate;
+    }
+
+    @Bean
+    public MessageConverter jacksonConvertor(){
+        return new Jackson2JsonMessageConverter();
+    }
+}
