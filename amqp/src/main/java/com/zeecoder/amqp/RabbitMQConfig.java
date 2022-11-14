0ae3d2cc531830eee38405d93e7521f154f7a2e3
ihor <i.zaporozhtsev@gmail.com>
2022-11-14 10:44:40 +0200

    add SimpleRabbitListenerContainerFactory

--- amqp/src/main/java/com/zeecoder/amqp/RabbitMQConfig.java
+++ amqp/src/main/java/com/zeecoder/amqp/RabbitMQConfig.java
@@ -2,6 +2,7 @@ package com.zeecoder.amqp;
 
 import lombok.AllArgsConstructor;
 import org.springframework.amqp.core.AmqpTemplate;
+import org.springframework.amqp.rabbit.config.SimpleRabbitListenerContainerFactory;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
@@ -15,14 +16,22 @@ public class RabbitMQConfig {
 
     private final ConnectionFactory connectionFactory;
 
-    @Bean
+    @Bean //allows us to send (produce) message to the queue
     public AmqpTemplate amqpTemplate(){
         RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
         rabbitTemplate.setMessageConverter(jacksonConvertor());
         return rabbitTemplate;
     }
 
-    @Bean
+    @Bean //allows us to receive (consume) message to the queue
+    public SimpleRabbitListenerContainerFactory containerFactory(){
+        SimpleRabbitListenerContainerFactory factory = new SimpleRabbitListenerContainerFactory();
+        factory.setConnectionFactory(connectionFactory);
+        factory.setMessageConverter(jacksonConvertor());
+        return factory;
+    }
+
+    @Bean // jackson converter
     public MessageConverter jacksonConvertor(){
         return new Jackson2JsonMessageConverter();
     }
