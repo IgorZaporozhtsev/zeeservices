12859c3879f5fa5c13d7be06b175723c1196282e
ihor <i.zaporozhtsev@gmail.com>
2022-11-17 08:36:12 +0200

    FOR TESTING PURPOSE
     commandLineRunner, scanBasePackages for testing purpose. Publish message directly from Notification service

--- notification/src/main/java/com/zeecoder/notification/NotificationApplication.java
+++ notification/src/main/java/com/zeecoder/notification/NotificationApplication.java
@@ -1,14 +1,32 @@
 package com.zeecoder.notification;
 
+import com.zeecoder.amqp.RabbitMQMessageProducer;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
 
-@SpringBootApplication
 @EnableEurekaClient
+@SpringBootApplication(
+        scanBasePackages = {
+                "com.zeecoder.notification",
+                "com.zeecoder.amqp"
+        }
+)
 public class NotificationApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(NotificationApplication.class, args);
+    }
+
+
+    // commandLineRunner, scanBasePackages for testing purpose. Publish message directly from Notification service
+
+    @Bean
+    CommandLineRunner commandLineRunner(RabbitMQMessageProducer producer, NotificationConfig config){
+        return args -> {
+            producer.publish("foo", config.getInternalExchange(), config.getInternalNotificationRoutingKey());
+        };
     }
 }
